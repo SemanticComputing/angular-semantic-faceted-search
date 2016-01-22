0eefc26626ef132362c89176656e09c8f798e00e
Mikko Koho <mikko.koho@iki.fi>
2016-01-22 13:48:37 +0200

    facetSelector directive

--- facetedsearch.js
+++ facetedsearch.js
@@ -43,11 +43,7 @@ facetApp.factory( 'Facets', function( $rootScope, $q, SparqlService, facetMapper
         }
 
         this.getStates = function(facetSelections) {
-            console.log(facetSelections);
             var query = ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#>' +
-                        ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>' +
-                        ' PREFIX m: <http://ldf.fi/sotasampo/narc/menehtyneet/>' +
-                        ' PREFIX m_schema: <http://ldf.fi/schema/narc-menehtyneet1939-45/>' +
                         ' PREFIX sf: <http://ldf.fi/functions#>' +
 
                         ' SELECT ?cnt ?id ?facet_text ?value WHERE {' +
@@ -103,20 +99,10 @@ facetApp.factory( 'Facets', function( $rootScope, $q, SparqlService, facetMapper
  */
 facetApp.controller( 'FacetListCtrl', function ( $scope, Facets ) {
     var vm = this;
-    
-    vm.facets = {
-        '<http://ldf.fi/schema/narc-menehtyneet1939-45/ammatti>': { name: 'Ammatti' },
-        '<http://ldf.fi/schema/narc-menehtyneet1939-45/asuinkunta>': { name: 'Asuinkunta' },
-        '<http://ldf.fi/schema/narc-menehtyneet1939-45/kansalaisuus>': { name: 'Kansalaisuus' },
-        '<http://ldf.fi/schema/narc-menehtyneet1939-45/kuolinkunta>': { name: 'Kuolinkunta' },
-        '<http://ldf.fi/schema/narc-menehtyneet1939-45/osasto>': { name: 'Joukko-osasto' },
-    };
 
-    //vm.facetStates = [];
+    vm.facets = $scope.facets;
 
-    vm.facetHandler = new Facets('http://ldf.fi/warsa/sparql', vm.facets);
-
-//    vm.facetHandler.getStates().then( function( states ) { vm.facetStates = states; });
+    vm.facetHandler = new Facets($scope.endpointUrl, $scope.facets);
 
     vm.getFacetSize = function( facetStates ) {
         if (facetStates) {
@@ -134,20 +120,141 @@ facetApp.controller( 'FacetListCtrl', function ( $scope, Facets ) {
     }, true)
 });
 
+facetApp.directive('facetSelector', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            endpointUrl: '=',
+            facets: '='
+        },
+        controller: 'FacetListCtrl',
+        controllerAs: 'vm',
+        templateUrl: 'facet.directive.html'
+    };
+});
+
+
+
 /*
  * Result handler service.
  */
-facetApp.factory( 'Results', [ '$rootScope', function( $rootScope ) {
-    var service = {
+facetApp.factory( 'Results', function( $rootScope, $q, SparqlService, facetMapperService ) {
+    return function( endpoint_url, properties ) {
 
-    }
+        var SPARQL = new SparqlService(endpoint_url);
 
-    return service;
-}]);
+        this.getResults = function(facetSelections) {
+            var query =
+                ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#>' +
+                ' PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>' +
+                ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' +
+                ' PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>' +
+                ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>' +
+                ' PREFIX owl:  <http://www.w3.org/2002/07/owl#>' +
+                ' PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>' +
+                ' PREFIX georss: <http://www.georss.org/georss/>' +
+                ' PREFIX text: <http://jena.apache.org/text#>' +
+                ' PREFIX m: <http://ldf.fi/sotasampo/narc/menehtyneet/>' +
+                ' PREFIX m_schema: <http://ldf.fi/schema/narc-menehtyneet1939-45/>' +
+
+                ' SELECT ?uri ?name ' +
+                Object.values( properties ).join(' ') +
+
+                ' WHERE {' +
+                ' GRAPH <http://ldf.fi/narc-menehtyneet1939-45/> {' +
+                ' ?uri a foaf:Person .' +
+                ' ?uri skos:prefLabel ?name .' +
+
+//                ' %emptyValue(NAME_STR) ? "" : "FILTER REGEX(?nimi, \"" + NAME_STR + "\", \"i\")"%' +
+//                ' %emptyValue(MARITALSTATUS) ? "" : "?uri m_schema:siviilisaeaety <"+ MARITALSTATUS +"> ."%' +
+//                ' %emptyValue(GENDER) ? "" : "?uri m_schema:sukupuoli <"+ GENDER +"> ."%' +
+//                ' %emptyValue(CHILDREN) ? "" : "?uri m_schema:lasten_lukumaeaerae "+ CHILDREN +" ."%' +
+//                ' %emptyValue(NATIONALITY) ? "" : "?uri m_schema:kansalaisuus <"+ NATIONALITY +"> ."%' +
+//                ' %emptyValue(RANK) ? "" : "?uri m_schema:sotilasarvo <"+ RANK +"> ."%' +
+//                ' %emptyValue(UNIT) ? "" : "?uri m_schema:osasto <"+ UNIT +"> ."%' +
+//                ' %(emptyValue(NATIONALITY) && emptyValue(CHILDREN) && emptyValue(GENDER) && emptyValue(MARITALSTATUS) && emptyValue(RANK) && emptyValue(UNIT) && emptyValue(NAME_STR)) ? "FILTER(STRSTARTS(?nimi, \"AA\"))" : ""%' +
+
+                ' ?uri m_schema:lasten_lukumaeaerae 5 .'
+
+//                ' OPTIONAL {' +
+//                ' ?uri m_schema:siviilisaeaety ?siviilisaeaetyuri .' +
+//                ' ?siviilisaeaetyuri skos:prefLabel ?siviilisaeaety . }' +
+//                ' OPTIONAL { ?uri m_schema:sukupuoli ?sukupuoliuri .' +
+//                ' ?sukupuoliuri skos:prefLabel ?sukupuoli . }' +
+//                ' OPTIONAL { ?uri m_schema:lasten_lukumaeaerae ?lasten_lukumaeaerae . }' +
+//                ' OPTIONAL { ?uri m_schema:kansalaisuus ?kansalaisuusuri .' +
+//                ' ?kansalaisuusuri skos:prefLabel ?kansalaisuus . }' +
+//                ' OPTIONAL { ?uri m_schema:aeidinkieli ?aeidinkieliuri .' +
+//                ' ?aeidinkieliuri skos:prefLabel ?aeidinkieli . }' +
+//                ' OPTIONAL { ?uri m_schema:menehtymisluokka ?menehtymisluokkauri .' +
+//                ' ?menehtymisluokkauri skos:prefLabel ?menehtymisluokka  . }' +
+//                ' OPTIONAL { ?uri m_schema:kuolinkunta ?kuolinkunta_uri .' +
+//                ' OPTIONAL {' +
+//                ' 	GRAPH <http://ldf.fi/warsa/places/municipalities> {' +
+//                ' 		?kuolinkunta_uri skos:prefLabel ?kuolinkunta_warsa .' +
+//                ' 		# OPTIONAL {' +
+//                ' 		# 	?kuolinkunta_uri wgs84:lat ?kuolinkunta_lat .' +
+//                ' 		# 	?kuolinkunta_uri wgs84:long ?kuolinkunta_long .' +
+//                ' 		# }' +
+//                ' 	}' +
+//                ' } OPTIONAL {' +
+//                ' 	?kuolinkunta_uri skos:prefLabel ?kuolinkunta_narc .' +
+//                ' 	# OPTIONAL {	?kuolinkunta_uri georss:point ?kuolinkunta_point . }' +
+//                ' }' +
+//                ' }' +
+//                ' OPTIONAL { ?uri m_schema:kuolinaika ?kuolinaika .' +
+//
+//                ' BIND(CONCAT("<a href=\"http://www.sotasampo.fi/times/page?uri=http://ldf.fi/warsa/events/times/time_",STR(?kuolinaika),"-",STR(?kuolinaika),"\">",STR(?kuolinaika),"</a>") AS ?kuolinpaiva) .' +
+//                ' }' +
+                ' OPTIONAL { ?uri m_schema:ammatti ?ammatti . }' +
+//                ' OPTIONAL { ?uri m_schema:kuolinpaikka ?kuolinpaikka . }' +
+//                ' OPTIONAL { ?uri m_schema:sotilasarvo ?arvouri .' +
+//                ' GRAPH <http://ldf.fi/warsa/actors/actor_types> {' +
+//                ' ?arvouri skos:prefLabel ?sotilasarvo  .' +
+//                ' }' +
+//                ' }' +
+//                ' OPTIONAL { ?uri m_schema:osasto ?osastouri .' +
+//                ' GRAPH <http://ldf.fi/warsa/actors> {' +
+//                ' ?osastouri skos:prefLabel ?osasto  .' +
+//                ' BIND(CONCAT("<a href=\"http://www.sotasampo.fi/page?uri=",STR(?osastouri),"\">",?osasto,"</a>") AS ?joukkoosastoHTML) .' +
+//                ' }' +
+//                ' }' +
+//
+                ' }' +
+//                ' BIND(COALESCE(?kuolinkunta_warsa, ?kuolinkunta_narc) as ?kuolinkunta)' +
+                ' }' +
+                ' GROUP BY ?uri ?nimi ?aeidinkieli ?lasten_lukumaeaerae ?kansalaisuus ?menehtymisluokka ?kuolinaika ?kuolinkunta ?point ?kuolinpaikka ?sukupuoli ?siviilisaeaety ?ammatti ?aeidinkieliuri ?lasten_lukumaeaeraeuri ?kansalaisuusuri ?siviilisaeaetyuri ?menehtymisluokkauri ?sukupuoliuri ?kuolinpaiva ?sotilasarvo ?arvouri' +
+                ' ORDER BY ?nimi';
+
+            var promise = SPARQL.getObjects(query);
+            return promise;
+        };
+    };
+});
 
 /*
  * Controller for the results view.
  */
-facetApp.controller( 'MainCtrl', function ( $scope ) {
+facetApp.controller( 'MainCtrl', function ( $scope, Results ) {
+    var vm = this;
+
+    vm.facets = {
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/ammatti>': { name: 'Ammatti' },
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/asuinkunta>': { name: 'Asuinkunta' },
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/kansalaisuus>': { name: 'Kansalaisuus' },
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/kuolinkunta>': { name: 'Kuolinkunta' },
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/osasto>': { name: 'Joukko-osasto' },
+    };
+
+    vm.properties = {
+        '<http://ldf.fi/schema/narc-menehtyneet1939-45/ammatti>': { name: 'ammatti' },
+    };
+
+    vm.endpoint_url = 'http://ldf.fi/warsa/sparql';
+
+    vm.FacetClass = '<http://xmlns.com/foaf/0.1/Person>';
+
+    vm.facetHandler = new Results('http://ldf.fi/warsa/sparql', vm.properties);
+
 });
 
