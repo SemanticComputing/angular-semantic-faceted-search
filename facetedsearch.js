5e3bcc34fb98eb8f333ca0998e467a08e8277c8c
Erkki Heino <erkki.heino@aalto.fi>
2016-02-01 17:10:08 +0200

    Make facet query template more legible

--- facetedsearch.js
+++ facetedsearch.js
@@ -49,36 +49,32 @@
 
             var facetStates = [];
             var endpoint = new SparqlService(endpointUrl);
-            var queryTemplate = ''+
+            var queryTemplate = '' +
                 ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#>' +
                 ' PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>' +
                 ' PREFIX sf: <http://ldf.fi/functions#>' +
 
                 ' SELECT ?cnt ?id ?facet_text ?value WHERE {' +
-                ' {' +
-                '   SELECT DISTINCT (count(DISTINCT ?s) as ?cnt) ?id ?value' +
-
-                '   WHERE {' +
-                '     VALUES ?id {' +
-
-                Object.keys( facets ).join(' ') +
-
-                '     }' +
-                (config.graph ? '     GRAPH ' + config.graph + ' { ' : '') +
-                (config.rdfClass ? ' ?s a ' + config.rdfClass + ' . ' : '') +
-
-                ' <SELECTIONS> ' +
-
-                '       ?s ?id ?value .' +
-                (config.graph ? ' } ' : '') +
-                '   } GROUP BY ?id ?value' +
-                ' }' +
+                '   {' +
+                '     SELECT DISTINCT (count(DISTINCT ?s) as ?cnt) ?id ?value' +
+                '     WHERE {' +
+                '       VALUES ?id {' +
+                '         <FACETS> ' +
+                '       } ' +
+                '       <GRAPH_START> ' +
+                '         <CLASS> ' +
+                '         <SELECTIONS> ' +
+                '         ?s ?id ?value .' +
+                '       <GRAPH_END> ' +
+                '     } GROUP BY ?id ?value' +
+                '   }' +
                 '   OPTIONAL {' +
-                '     ?value sf:preferredLanguageLiteral (skos:prefLabel "' + (config.preferredLang ? config.preferredLang : 'fi') + '" "" ?lbl) .' +
+                '     ?value sf:preferredLanguageLiteral (skos:prefLabel "<PREF_LANG>" "" ?lbl) .' +
                 '   }' +
                 '   BIND(COALESCE(?lbl, ?value) as ?facet_text)' +
                 ' }' +
                 ' ORDER BY ?id ?facet_text';
+            queryTemplate = buildQueryTemplate(queryTemplate);
 
 
             function getStates(facetSelections) {
@@ -96,6 +92,35 @@
             function buildQuery(facetSelections) {
                 return queryTemplate.replace('<SELECTIONS>',
                         facetSelectionFormatter.parseFacetSelections(facetSelections));
+            }
+
+            function buildQueryTemplate(template) {
+                var templateSubs = [
+                    {
+                        placeHolder: '<FACETS>',
+                        value: Object.keys( facets ).join(' ')
+                    },
+                    {
+                        placeHolder: '<GRAPH_START>',
+                        value: (config.graph ? '     GRAPH ' + config.graph + ' { ' : '')
+                    },
+                    {
+                        placeHolder: '<CLASS>',
+                        value: (config.rdfClass ? ' ?s a ' + config.rdfClass + ' . ' : '')
+                    },
+                    {
+                        placeHolder: '<GRAPH_END>',
+                        value: (config.graph ? ' } ' : '') },
+                    {
+                        placeHolder: '<PREF_LANG>',
+                        value: (config.preferredLang ? config.preferredLang : 'fi')
+                    }
+                ];
+
+                templateSubs.forEach(function(s) {
+                    template = template.replace(s.placeHolder, s.value);
+                });
+                return template;
             }
         };
     })
