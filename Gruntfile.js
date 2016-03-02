3530516b56ec67fbb665290355cee30bb3c816b3
Erkki Heino <erkki.heino@aalto.fi>
2016-03-02 13:55:33 +0200

    Create a bower package, and remove implementations

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        ngtemplates: {
+            app: {
+                src: ['src/facets/facets.directive.html'],
+                dest: 'dist/templates.js'
+            }
+        },
+        concat: {
+            dist: {
+                src: [
+                    'src/facets/facets.module.js',
+                    'src/facets/facets.facet-mapper-service.js',
+                    'src/facets/facets.service.js',
+                    'src/facets/facets.selection-formatter.js',
+                    'src/facets/facets.directive.js',
+                    'src/facets/facets.text-with-selection.filter.js',
+                    'src/faceturlstate/faceturlstate.module.js',
+                    'src/faceturlstate/faceturlstate.url-state-handler-service.js',
+                    'src/results/results.service.js',
+                    'dist/templates.js'
+                ],
+                dest: 'dist/sparql-service.js'
+            }
+        },
+        clean: {
+            templates: ['dist/templates.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-angular-templates');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build', [
+        'ngtemplates',
+        'concat:dist',
+        'clean:templates'
+    ]);
+};
