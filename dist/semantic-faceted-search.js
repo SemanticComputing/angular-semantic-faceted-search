228fc98732646e42bf6f00044f6f165dd73c6f24
Erkki Heino <erkki.heino@aalto.fi>
2016-10-21 17:36:47 +0300

    Fix build

--- dist/semantic-faceted-search.js
+++ dist/semantic-faceted-search.js
@@ -838,81 +838,6 @@
     }
 })();
 
-/*
-* Facet for selecting a simple value.
-*/
-(function() {
-    'use strict';
-
-    angular.module('seco.facetedSearch')
-
-    .factory('BasicFacet', BasicFacet);
-
-    /* ngInject */
-    function BasicFacet(_, AbstractFacet) {
-
-        return BasicFacetConstructor;
-
-        function BasicFacetConstructor(options) {
-            var self = this;
-
-            /* Public API */
-
-            self.disable = disable;
-            self.enable = enable;
-            self.isLoading = isLoading;
-            self.isEnabled = isEnabled;
-            self.getSelectedValue = getSelectedValue;
-
-            // Properties
-            self.selectedValue = {};
-
-            /* Implementation */
-
-            init(options);
-
-            function init(options) {
-                // Initial value
-                var constVal = options.initialConstraints.facets[self.facetUri];
-                if (constVal && constVal.value) {
-                    self.selectedValue = { value: constVal.value };
-                }
-
-                self = angular.extend(self, new AbstractFacet(self, options));
-            }
-
-            /* Public API functions */
-
-            function getSelectedValue() {
-                var val;
-                if (_.isArray(self.selectedValue)) {
-                    val = _.map(self.selectedValue, 'value');
-                } else {
-                    val = self.selectedValue.value;
-                }
-                return val;
-            }
-
-            function isEnabled() {
-                return self._isEnabled;
-            }
-
-            function enable() {
-                self._isEnabled = true;
-            }
-
-            function disable() {
-                self.selectedValue = {};
-                self._isEnabled = false;
-            }
-
-            function isLoading() {
-                return self.isBusy();
-            }
-        }
-    }
-})();
-
 (function() {
     'use strict';
 
@@ -1044,6 +969,81 @@
                 return Math.min(facetStates.length + 2, 10).toString();
             }
             return '10';
+        }
+    }
+})();
+
+/*
+* Facet for selecting a simple value.
+*/
+(function() {
+    'use strict';
+
+    angular.module('seco.facetedSearch')
+
+    .factory('BasicFacet', BasicFacet);
+
+    /* ngInject */
+    function BasicFacet(_, AbstractFacet) {
+
+        return BasicFacetConstructor;
+
+        function BasicFacetConstructor(options) {
+            var self = this;
+
+            /* Public API */
+
+            self.disable = disable;
+            self.enable = enable;
+            self.isLoading = isLoading;
+            self.isEnabled = isEnabled;
+            self.getSelectedValue = getSelectedValue;
+
+            // Properties
+            self.selectedValue = {};
+
+            /* Implementation */
+
+            init(options);
+
+            function init(options) {
+                // Initial value
+                var constVal = options.initialConstraints.facets[self.facetUri];
+                if (constVal && constVal.value) {
+                    self.selectedValue = { value: constVal.value };
+                }
+
+                self = angular.extend(self, new AbstractFacet(self, options));
+            }
+
+            /* Public API functions */
+
+            function getSelectedValue() {
+                var val;
+                if (_.isArray(self.selectedValue)) {
+                    val = _.map(self.selectedValue, 'value');
+                } else {
+                    val = self.selectedValue.value;
+                }
+                return val;
+            }
+
+            function isEnabled() {
+                return self._isEnabled;
+            }
+
+            function enable() {
+                self._isEnabled = true;
+            }
+
+            function disable() {
+                self.selectedValue = {};
+                self._isEnabled = false;
+            }
+
+            function isLoading() {
+                return self.isBusy();
+            }
         }
     }
 })();
