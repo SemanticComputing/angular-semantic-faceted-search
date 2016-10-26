f337bae9a2b1192f132df9f226e9fb6cb3f3702a
Erkki Heino <erkki.heino@aalto.fi>
2016-10-26 14:02:29 +0300

    Documentation (FacetHandler)

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -11,6 +11,15 @@ NG_DOCS={
       "moduleName": "index",
       "shortDescription": "SPARQL Faceter -- Faceted Search Based on SPARQL",
       "keywords": "$on $scope ammatti api argument attribute based basic basicfacet birth birthmunicipality birthplace box broadcasting built-in change changes cidoc-crm classes collection common configuration configured constraint constraints controller custom data-options dataservice death deathdate default define defines directive directives displayed documentation easily emits emitted enabled endpointurl endpredicate equalto event events facet faceted facetedsearch faceter facethandler facetid facets facetstate false fetches fi fields filtering free-text friendly function generated getresults handler handles hierarchical hierarchy hierarchyfacet http implemented individual init initial initialize initlistener interested key labels listen listening listens maintaining max mediates min module municipality muninn-project narrows objectfollowing occupation options org overview path pattern predicate preferredlang preflabel presumably principalabode profession property range rank rdfclass reasonably responsible resultset scope search seco select selections service services set setup sf-facet-constraints sf-facets-changed sf-initial-constraints skos source sparql startpredicate support synchronizes template text textfacet time timespanfacet title triple true types unique update updateresults updates usable users values var variable vm w3 work works"
+    },
+    {
+      "section": "api",
+      "id": "seco.facetedSearch.FacetHandler",
+      "shortName": "seco.facetedSearch.FacetHandler",
+      "type": "service",
+      "moduleName": "seco.facetedSearch",
+      "shortDescription": "FacetHandler",
+      "keywords": "$rootscope api argument broadcast broadcasting broadcasts built-in case collection communication config configuration constants constraint constraints created currently default defined directly emitted en endpoint event event_facet_changed event_facet_constraints event_initial_constraints event_request_constraints events example extra facet faceted facetedsearch facethandler facetid facets faceturlstatehandlerservice fields generated included individual individually init initial injectable instantiating key label labels language listeners listening listens loading mediating method names needed object omitting optional parameters parent pattern preferred properties rdf reason remove removelisteners resource resources response scope seco selects service sf-facet-changed sf-facet-constraints sf-facets-changed sf-initial-constraints sf-request-constraints shorthand sparql structure tag triple uri url values"
     }
   ],
   "apis": {
