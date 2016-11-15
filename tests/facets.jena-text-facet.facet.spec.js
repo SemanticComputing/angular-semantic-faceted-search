038fb3ee92d50905b4e4c522322b5919c45ba183
Erkki Heino <erkki.heino@aalto.fi>
2016-11-15 10:47:46 +0200

    secoJenaTextFacet tests

--- tests/facets.jena-text-facet.facet.spec.js
+++ tests/facets.jena-text-facet.facet.spec.js
@@ -2,7 +2,7 @@
 /* global inject, module  */
 
 describe('JenaTextFacet', function() {
-    var JenaTextFacet, facet, options;
+    var JenaTextFacet, facet, options, subPred;
 
     beforeEach(module('seco.facetedSearch'));
     beforeEach(inject(function(_JenaTextFacet_) {
@@ -16,6 +16,8 @@ describe('JenaTextFacet', function() {
         };
 
         facet = new JenaTextFacet(options);
+
+        subPred = '?id <http://jena.apache.org/text#query> ';
 
     }));
 
@@ -96,30 +98,90 @@ describe('JenaTextFacet', function() {
             expect(isIncluded).toBe(true);
         });
 
-        it('should work with or without a predicate', function() {
+        it('should return a valid constraint if no property is given', function() {
             var searchTerms = 'foo bar';
-            var pred = '<http://www.w3.org/2004/02/skos/core#prefLabel>';
 
             facet.selectedValue = searchTerms;
-            var cons = facet.getConstraint();
+            var cons = facet.getConstraint().trim();
 
-            var isIncluded = (cons.indexOf(searchTerms) > -1);
-            expect(isIncluded).toBe(true);
+            var expected = subPred + '("foo bar") .';
 
-            isIncluded = (cons.indexOf(pred) > -1);
-            expect(isIncluded).toBe(false);
+            expect(cons).toEqual(expected);
+        });
+
+        it('should return a valid constraint if a property is given', function() {
+            var searchTerms = 'foo bar';
+            var pred = '<pred>';
 
             options.predicate = pred;
             facet = new JenaTextFacet(options);
 
             facet.selectedValue = searchTerms;
-            cons = facet.getConstraint();
+            var cons = facet.getConstraint().trim();
 
-            isIncluded = (cons.indexOf(searchTerms) > -1);
-            expect(isIncluded).toBe(true);
+            var expected = subPred + '(<pred> "foo bar") .';
 
-            isIncluded = (cons.indexOf(pred) > -1);
-            expect(isIncluded).toBe(true);
+            expect(cons).toEqual(expected);
+        });
+
+        it('should use the limit if given', function() {
+            var searchTerms = 'foo bar';
+            var pred = '<pred>';
+
+            options.limit = 10;
+            facet = new JenaTextFacet(options);
+
+            facet.selectedValue = searchTerms;
+            var cons = facet.getConstraint().trim();
+
+            var expected = subPred + '("foo bar" 10) .';
+
+            expect(cons).toEqual(expected);
+
+            options.predicate = pred;
+            facet = new JenaTextFacet(options);
+
+            facet.selectedValue = searchTerms;
+            cons = facet.getConstraint().trim();
+
+            expected = subPred + '(<pred> "foo bar" 10) .';
+
+            expect(cons).toEqual(expected);
+        });
+
+        it('should use the graph if given', function() {
+            var searchTerms = 'foo bar';
+            var pred = '<pred>';
+
+            options.graph = '<graph>';
+            facet = new JenaTextFacet(options);
+
+            facet.selectedValue = searchTerms;
+            var cons = facet.getConstraint().trim().replace(/\s+/g, ' ');
+
+            var expected = 'GRAPH <graph> { ' + subPred + '("foo bar") . }';
+
+            expect(cons).toEqual(expected);
+
+            options.predicate = pred;
+            facet = new JenaTextFacet(options);
+
+            facet.selectedValue = searchTerms;
+            cons = facet.getConstraint().trim().replace(/\s+/g, ' ');
+
+            expected = 'GRAPH <graph> { ' + subPred + '(<pred> "foo bar") . }';
+
+            expect(cons).toEqual(expected);
+
+            options.limit = 100;
+            facet = new JenaTextFacet(options);
+
+            facet.selectedValue = searchTerms;
+            cons = facet.getConstraint().trim().replace(/\s+/g, ' ');
+
+            expected = 'GRAPH <graph> { ' + subPred + '(<pred> "foo bar" 100) . }';
+
+            expect(cons).toEqual(expected);
         });
     });
 });
