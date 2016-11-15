6bed5eae911d59945aff8e9bb0a7013f41b5e322
Erkki Heino <erkki.heino@aalto.fi>
2016-11-15 15:59:52 +0200

    Update secoJenaTextFacet documentation

--- src/facets/jena-text/facets.jena-text-facet.directive.js
+++ src/facets/jena-text/facets.jena-text-facet.directive.js
@@ -12,12 +12,12 @@
     * This facet can only be used if the SPARQL endpoint supports
     * [Jena text query](https://jena.apache.org/documentation/query/text-query.html).
     *
-    * The produced constraint looks like this:
+    * The produced constraint looks like the following
+    * (where `predicate`, and `limit` are based on the configuration options,
+    * and left out if undefined):
     * <pre>
     * (?id ?score) <http://jena.apache.org/text#query> (predicate "search terms" limit) .
     * </pre>
-    * where `predicate`, and `limit` are based on the configuration options
-    * (and left out if they are undefined).
     *
     * If the `graph` option is defined, the constraint is wrapped accordingly:
     * <pre>
@@ -25,6 +25,12 @@
     *   (?id ?score) <http://jena.apache.org/text#query> (predicate "search terms" limit) .
     * }
     * </pre>
+    *
+    * The score is captured as variable `?score`, and can thus be used to sort results.
+    *
+    * In case there is an even number of quotes (`"`) they are escaped in the search terms.
+    * If there is an odd number of quotes, they are removed.
+    * Otherwise the terms are not modified.
     *
     * Does not make any SPARQL queries, just generates SPARQL triple patterns
     * out of the typed text for other facets to use.
@@ -35,9 +41,9 @@
     * - **name** - `{string}` - The title of the facet. Will be displayed to end users.
     * - **[enabled]** `{boolean}` - Whether or not the facet is enabled by default.
     *   If undefined, the facet will be disabled by default.
+    * - **[priority]** - `{number}` - Priority for constraint sorting. Default is 0.
     * - **[predicate]** - `{string}` - The property to use in the search.
     *   See [Jena text query documentation](https://jena.apache.org/documentation/query/text-query.html#query-with-sparql).
-    * - **[priority]** - `{number}` - Priority for constraint sorting. Default is 0.
     * - **[limit]** - `{number}` - Limit for the text search results.
     *   See [Jena text query documentation](https://jena.apache.org/documentation/query/text-query.html#query-with-sparql).
     * - **[graph]** - `{string}` - The URI of the graph to use for the text search.
