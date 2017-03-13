d5c3307c3ac5f2a3a3b82e23cf43716e0664c056
Erkki Heino <erkki.heino@aalto.fi>
2017-03-13 11:53:05 +0200

    Check for multiple labels for the same value

--- src/facets/facets.facet-mapper-service.js
+++ src/facets/facets.facet-mapper-service.js
@@ -13,6 +13,7 @@
     /* ngInject */
     function facetMapperService(_, objectMapperService) {
         FacetMapper.prototype.makeObject = makeObject;
+        FacetMapper.prototype.makeObjectListNoGrouping = makeObjectListNoGrouping;
 
         var proto = Object.getPrototypeOf(objectMapperService);
         FacetMapper.prototype = angular.extend({}, proto, FacetMapper.prototype);
@@ -31,6 +32,22 @@
             o.count = parseInt(obj.cnt.value);
 
             return o;
+        }
+
+        function makeObjectListNoGrouping(objects) {
+            var self = this;
+            var obj_list = _.transform(objects, function(result, obj) {
+                obj = self.makeObject(obj);
+                var last = _.last(result);
+                if (last && last.value === obj.value) {
+                    if (obj.facet_text) {
+                        last.facet_text = last.facet_text ? last.face_text + ', ' + obj.face_text : obj.face_text;
+                    }
+                } else {
+                    result.push(obj);
+                }
+            });
+            return obj_list;
         }
 
         function parseValue(value) {
